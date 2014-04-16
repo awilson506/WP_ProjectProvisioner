2d9dfe074e54b2bf183be4df4d773d91ab375cc5
Ciaran McGrath <ciaranmg@gmail.com>
2014-04-16 14:41:08 -0400

    edits to provisioner

--- vagrant_bootstrap.sh
+++ vagrant_bootstrap.sh
@@ -1,35 +1,69 @@
 #!/usr/bin/env bash
 
 # Configuration
-echo "--- Setting configuration variables ---"
+echo "
+---------------------------------------------------------------------------------
+|                                                                               |
+|                         Setting configuration variables                       |
+|                                                                               |
+---------------------------------------------------------------------------------
+"
 
 source config.sh
 
-echo "--- Updating packages list ---"
-sudo apt-get update
+echo "
+---------------------------------------------------------------------------------
+|                                                                               |
+|                          Updating packages list                               |
+|                                                                               |
+---------------------------------------------------------------------------------
+"
+sudo touch /home/vagrant/provision_log.log
+sudo apt-get update >> /home/vagrant/provision_log.log
 
-echo "--- Set up MySQL ---"
+echo "
+---------------------------------------------------------------------------------
+|                                                                               |
+|                              Configure MySQL                                  |
+|                                                                               |
+---------------------------------------------------------------------------------
+"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASS"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASS"
 
 
-echo "--- Installing base packages ---"
-sudo apt-get install -y vim curl python-software-properties
+echo "
+---------------------------------------------------------------------------------
+|                                                                               |
+|                          Installing base packages                             |
+|                                                                               |
+---------------------------------------------------------------------------------
+"
+sudo apt-get install -y vim curl python-software-properties >> /home/vagrant/provision_log.log
 
-echo "--- Updating packages list ---"
-sudo apt-get update
+echo "
+---------------------------------------------------------------------------------
+|                                                                               |
+|                          Updating packages list                               |
+|                                                                               |
+---------------------------------------------------------------------------------
+"
+sudo apt-get update >> /home/vagrant/provision_log.log
 
-echo "--- We want the bleeding edge of PHP ---"
-sudo add-apt-repository -y ppa:ondrej/php5
+sudo add-apt-repository -y ppa:ondrej/php5 >> /home/vagrant/provision_log.log
 
-echo "--- Updating packages list ---"
-sudo apt-get update
+sudo apt-get update >> /home/vagrant/provision_log.log
 
-echo "--- Installing PHP-specific packages ---"
-sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core zsh
+echo "
+---------------------------------------------------------------------------------
+|                                                                               |
+|                        Installing PHP and Xdebug                              |
+|                                                                               |
+---------------------------------------------------------------------------------
+"
+sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql git-core zsh >> /home/vagrant/provision_log.log
 
-echo "--- Installing and configuring Xdebug ---"
-sudo apt-get install -y php5-xdebug
+sudo apt-get install -y php5-xdebug >> /home/vagrant/provision_log.log
 
 cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
 xdebug.cli_color=1
@@ -44,16 +78,16 @@ xdebug.remote_log="/home/vagrant/xdebug.log"
 xdebug.remote_host=10.0.2.2 ; IDE-Environments IP, from vagrant box.
 EOF
 
-echo "--- Enabling mod-rewrite ---"
+echo "
+---------------------------------------------------------------------------------
+|                                                                               |
+|                             Setting up Apache                                 |
+|                                                                               |
+---------------------------------------------------------------------------------
+"
+
 sudo a2enmod rewrite
 
-# echo "--- Setting document root ---"
-# sudo sudo rm -rf /var/www
-# sudo sudo ln -fs /vagrant/public /var/www
-
-echo "--- Enable colors on console --- "
-sed -i 's/#force_color_prompt/force_color_prompt/g' ~/.bashrc
-curl -L http://install.ohmyz.sh | sh
 
 echo "--- Set apache host name --- "
 cat << EOF | sudo tee /etc/apache2/httpd.conf
@@ -94,19 +128,50 @@ echo "--- Restarting Apache ---"
 sudo service apache2 restart
 
 
-echo "--- Shell Settings ---"
+
+echo "
+---------------------------------------------------------------------------------
+|                                                                               |
+|                            Configuring Terminal                               |
+|                                                                               |
+---------------------------------------------------------------------------------
+"
+
+sed -i 's/#force_color_prompt/force_color_prompt/g' ~/.bashrc
+sudo curl -L http://install.ohmyz.sh | sh
+
 export PATH=$PATH:vendor/bin
 
-echo "--- Configuring the database ---"
+
+echo "
+---------------------------------------------------------------------------------
+|                                                                               |
+|                            Configuring the database                           |
+|                                                                               |
+---------------------------------------------------------------------------------
+"
 echo "CREATE DATABASE ${DATABASE_NAME};" | mysql -u root -proot
 echo "GRANT USAGE ON *.* to ${DATABASE_USER}@localhost identified by '$DATABASE_PASSWORD';" | mysql -u root -p${MYSQL_ROOT_PASS}
 echo "GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* to ${DATABASE_USER}@localhost;" | mysql -u root -p${MYSQL_ROOT_PASS}
 
 if [ -f /home/vagrant/data/database_dump.sql ]; then
-    echo "--- Mounting the database ---"
+
+echo "
+---------------------------------------------------------------------------------
+|                                                                               |
+|                            Mounting the database                              |
+|                                                                               |
+---------------------------------------------------------------------------------
+"
     mysql -u root -p${MYSQL_ROOT_PASS} ${DATABASE_NAME} < /home/vagrant/data/database_dump.sql
 fi
 
+echo "
+---------------------------------------------------------------------------------
+|                                                                               |
+|                                    Done                                       |
+|          You will need to add $BOX_IP $DOMAIN to your hosts file              |
+|                                                                               |
+---------------------------------------------------------------------------------
+"
 
-echo "--- Done ---"
-echo "You will need to add $BOX_IP $DOMAIN to your hosts file"
