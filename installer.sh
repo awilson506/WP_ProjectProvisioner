905c2da2fd1196c252380ff4bf4e3065d3ab3f76
Ciaran McGrath <ciaranmg@gmail.com>
2014-04-16 14:44:33 -0400

    moved installer

---
+++ installer.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+
+echo  "
+__        ______    ____                 _     _                       
+\ \      / /  _ \  |  _ \ _ __ _____   _(_)___(_) ___  _ __   ___ _ __ 
+ \ \ /\ / /| |_) | | |_) | '__/ _ \ \ / / / __| |/ _ \| '_ \ / _ \ '__|
+  \ V  V / |  __/  |  __/| | | (_) \ V /| \__ \ | (_) | | | |  __/ |   
+   \_/\_/  |_|     |_|   |_|  \___/ \_/ |_|___/_|\___/|_| |_|\___|_| 
+"
+
+
+
+read -p "Enter Domain Name for your DEV environment: " DOMAIN
+echo " "
+
+if test "$DOMAIN" == ""; then
+	echo "$0: Domain is required " >&2
+	exit 1;
+fi
+
+
+
+read -p "Enter repository URL (Enter for none) " REPO
+echo " "
+
+if test "$REPO" != ""; then
+	echo "Cloning Project Repository"
+	git clone $REPO document_root
+else
+	mkdir document_root
+fi
+
+echo "Installing latest version of wordpress"
+git clone git@github.com:WordPress/WordPress.git document_root/wp
+
+read -p "Enter Database Name: " DBNAME
+echo " "
+
+read -p "Enter Database User: " DBUSER
+echo " "
+
+read -p "Enter Database Password: " DBPASS
+echo " "
+
+cat <<EOF | tee config.sh
+BOX_IP="192.168.50.6"
+DOMAIN="$DOMAIN"
+MYSQL_ROOT_PASS="root"
+DATABASE_NAME="$DBNAME"
+DATABASE_USER="$DBUSER"
+DATABASE_PASSWORD="$DBPASS"
+EOF
+
+
+echo "Creating data directory."
+mkdir data
+echo "
+---------------------------------------------------------------------------------
+|                                                                               |
+|   Remember to place a database_dump.sql file in data/ for a development DB    |
+|                                                                               |
+---------------------------------------------------------------------------------
+"
+
